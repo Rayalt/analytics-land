6398382782b7dbd86d164599d5aa71d6780c1343
Rayalt <krol.katerina@gmail.com>
2021-01-05 20:27:32 +0300

    remastered 5 jan

---
+++ dev/js/partials/user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+window.onload = function() {
+    let menuTrigger = document.querySelector('.header__menu'),
+        closeMenuButton = document.querySelector('.side-nav__close'),
+        darkerCurtain = document.querySelector('.page__darker'),
+        pageBody = document.querySelector('body');
+
+    menuTrigger.addEventListener('click', function() {
+        pageBody.classList.toggle('open-nav');
+    })
+    closeMenuButton.addEventListener('click', function() {
+        pageBody.classList.toggle('open-nav');
+    })
+    /*darkerCurtain.addEventListener('click', function() {
+        pageBody.classList.toggle('open-nav');
+    })*/
+}
